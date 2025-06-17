f8885409254001cdb08a9651f2a148e1146de06b
ZureTz <2819058133@qq.com>
2025-06-18 03:21:47 +0800

    chore: comment out rewrites and skipTrailingSlashRedirect in next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -6,23 +6,23 @@ const nextConfig: NextConfig = {
   // Generate static HTML for all pages
   // output: "export",
 
-  async rewrites() {
-    const backendUrl =
-      process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:8080";
+  // async rewrites() {
+  //   const backendUrl =
+  //     process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:8080";
 
-    return [
-      {
-        source: "/api/:path*",
-        destination: `${backendUrl}/api/:path*`,
-      },
-      {
-        source: "/:path*",
-        destination: `${backendUrl}/:path*`,
-      },
-    ];
-  },
-  // This is required to support PostHog trailing slash API requests
-  skipTrailingSlashRedirect: true,
+  //   return [
+  //     {
+  //       source: "/api/:path*",
+  //       destination: `${backendUrl}/api/:path*`,
+  //     },
+  //     {
+  //       source: "/:path*",
+  //       destination: `${backendUrl}/:path*`,
+  //     },
+  //   ];
+  // },
+  // // This is required to support PostHog trailing slash API requests
+  // skipTrailingSlashRedirect: true,
 };
 
 export default nextConfig;
